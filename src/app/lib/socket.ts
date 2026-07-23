f4731fc8c3199a409289e15f93b4ff401b4cc532
W7112011 <wasemgecob@gmail.com>
2026-07-23 21:14:37 +0300

    Edit socket.ts

--- src/app/lib/socket.ts
+++ src/app/lib/socket.ts
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
+export const socket = io(process.env.NEXT_PUBLIC_API_URL, {
   autoConnect: false,
+  transports: ["websocket", "polling"],
 });
